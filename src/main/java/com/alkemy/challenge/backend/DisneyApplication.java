0df3d397f02c10c1e0cbf4f3576e1b8372a56429
yasuaje <yeisonasuaje2010@gmail.com>
2022-03-27 19:38:56 -0400

    alkemy-challenge

--- src/main/java/com/alkemy/challenge/backend/DisneyApplication.java
+++ src/main/java/com/alkemy/challenge/backend/DisneyApplication.java
@@ -1,4 +1,4 @@
-package com.grupo.consiti.proyectoRodriguez;
+package com.alkemy.challenge.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
